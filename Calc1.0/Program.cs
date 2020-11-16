03739e3f213125f0b3d3a9984693257b5efb12bf
miglio000 <gabrielemiglio23@gmail.com>
2020-11-16 11:17:08 +0100

    Calc1.0 con Libreria

---
+++ Calc1.0/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Operazioni;
+namespace Calc1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1 = Calcoli.Variabile;
+            double n2 = Calcoli.Variabile;
+            string scelta = Calcoli.Scelta;
+
+        }
+    }
+}
---
+++ Operazioni/Calcoli.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Operazioni
+{
+    public class Calcoli
+    {
+        public static double Variabile(double n)
+        {
+            Console.WriteLine("Inserire il numero");
+            n = Convert.ToDouble(Console.ReadLine());
+            return n;
+        }
+        
+        }
+        public static double Scelta(double a, double b)
+        {
+            Console.WriteLine("Quale operazione vuoi eseguire?");
+            string scelta = Convert.ToString(Console.ReadLine());
+
+            /*switch (scelta)
+            {
+                case "+":
+                case "somma":
+                    double somma = a + b;
+                    return somma;
+                    break;
+                case "-":
+                case "differenza":
+                    double differenza = a - b;
+                    return differenza;
+                    break;
+                case "*":
+                case "moltiplicazione":
+                    double moltiplicazione = a * b;
+                    return moltiplicazione;
+                    break;
+                case "/":
+                case "divisione":
+                    double divisione = a / b;
+                    return divisione;
+                    break;
+                default:
+                    Console.WriteLine("Errore.");
+                    break;
+            }*/
+            if (scelta == "somma" || scelta == "+")
+            {
+                double somma = a + b;
+                return somma;
+            }else if(scelta =="differenza" || scelta == "-")
+            {
+                double differenza = a - b;
+                return differenza;
+            }else if(scelta == "moltiplicazione" || scelta == "*")
+            {
+                double moltiplicazione = a * b;
+                return moltiplicazione;
+            }else if(scelta == "divisione" || scelta == "/")
+            {
+                double divisione = a / b;
+                return divisione;
+            }
+        }
+    }
+}
